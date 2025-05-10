a0f3aa4679300cc94cc7616882ce1a3da8a94247
Aritix <122972707+Aritix@users.noreply.github.com>
2025-05-09 21:11:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log(Array.from(process.env.FLAG_GRAVY_OVERFLOW_L0_GRAVY).map(char => char.charCodeAt(0).toString(16).padStart(2, '0')).reverse().join(''))
+console.log(btoa(process.env.FLAG_GRAVY_OVERFLOW_L0_GRAVY))
